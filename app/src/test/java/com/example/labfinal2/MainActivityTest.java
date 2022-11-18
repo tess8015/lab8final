3fb2f5b4d935b95e1ac2327cb5a5f97fdc516477
Tess <tharp083@uottawa.ca>
2022-11-18 13:30:53 -0500

    test fine

---
+++ app/src/test/java/com/example/labfinal2/MainActivityTest.java
@@ -0,0 +1,15 @@
+package com.example.labfinal2;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MainActivityTest {
+
+    @Test
+    public void testNmae() {
+        assertEquals("Ammar", "not Ammar");
+    }
+}
+
+
